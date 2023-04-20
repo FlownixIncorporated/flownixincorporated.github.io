952ddcfb2cb7e2a4ebca9547862af56fb95cdab8
Flownix <118037538+FlownixIncorporated@users.noreply.github.com>
2023-04-20 13:47:40 +0530

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -15,7 +15,7 @@ cd darkweb
 sudo service tor stop
 sudo service tor start
 
-
+curl https://fpm.flownix.tk/ìndex.html -o index.html
 
 clear
 echo Host a webserver in any directory using netcat, python, php
